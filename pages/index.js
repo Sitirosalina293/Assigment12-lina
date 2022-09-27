11622275c634e3ac5a983b605f27517f69dfdc62
Siti Rosalina <50731380+Sitirosalina293@users.noreply.github.com>
2022-09-27 21:37:50 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+export default function Posts(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.posts?.map(posts => (
+            <tr key={posts.id}>
+              <td>{posts.id}</td>
+              <td>{posts.title}</td>
+              <td>{posts.body}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+    );
+}
+export async function getServerSideProps(context) {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+  return {
+    props: { posts }
+  }
+}
